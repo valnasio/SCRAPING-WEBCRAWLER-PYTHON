819cba3f9d4c024a9ad14cab2f3fd4f831228b56
valnasio <valnasio1@gmail.com>
2023-10-05 17:35:04 -0300

    WebCrawler  -  Buscando no site Boca Rosa

--- src/venv/crawler.py
+++ src/venv/crawler.py
@@ -1,40 +1,52 @@
+# Importe a função unidecode para lidar com caracteres acentuados
 from unidecode import unidecode
-import requests
-from bs4 import BeautifulSoup
-import re
 
+# Importe as bibliotecas necessárias
+import requests  # Para fazer solicitações HTTP
+from bs4 import BeautifulSoup  # Para analisar o HTML
+import re  # Para expressões regulares
+
+# Importe a biblioteca locale para configurar o local
 import locale
 
 # Configurar o local para o Brasil
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-#site:   https://www.bocarosadamakeup.com.br/
-
-
+# Defina a classe Crawler
 class Crawler:
 
-
-
+    # Método para fazer solicitação HTTP e retornar o HTML analisado
     def request_data(self, url: str):
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
         return soup
 
+    # Método para extrair dados do site
     def extract_from_site(self):
+        # Faça a solicitação HTTP e analise o HTML do site
         raw_site = self.request_data("https://www.bocarosadamakeup.com.br/")
 
+        # Encontre todas as divisões com a classe "item-container"
         products = raw_site.find_all("div", {"class": "item-container"})
 
+        # Itere sobre os produtos encontrados
         for product in products:
+            # Encontre a imagem do produto
             image = product.find("img", {"class": "ilazyloaded"})
+            
+            # Encontre o título do produto
             title = product.find("div", {"class": "item-name"})
+            
+            # Encontre o preço bruto do produto
             raw_price = product.find("div", {"class": "item-price"})
-         
 
+            # Verifique se há um preço bruto
             if raw_price:
+                # Use uma expressão regular para extrair o preço em formato "R$ x,xx"
                 pattern_price = r"R\$\s*([\d\.]+),(\d+)"
                 match = re.search(pattern_price, raw_price.text)
 
+                # Se houver uma correspondência, formate o preço com duas casas decimais
                 if match:
                     price = '{:,.2f}'.format(float(match.group(1).replace('.', '').replace(',', '.'))).replace(',', ',')
                 else:
@@ -42,18 +54,25 @@ class Crawler:
             else:
                 price = "Preço não encontrado"
 
+            # Crie um dicionário de dados com imagem, título e preço
             data = {
-                "Imagem": image.attrs('src') if image else "Imagem nao encontrada",
-                "Titulo": title.text.strip() if title else "Título nao encontrado",
-                "Preco R$":  price,
+                "Imagem": image.attrs['src'] if image else "Imagem não encontrada",
+                "Título": title.text.strip() if title else "Título não encontrado",
+                "Preço R$":  price,
             }
 
+            # Imprima os dados do produto
             print("Produto:")
             for key, value in data.items():
                 print(f"{key}: {value}")
 
+            # Imprima uma linha divisória para separar os produtos
             print("-" * 50) 
 
+# Verifique se o código está sendo executado como um script principal
 if __name__ == "__main__":
+    # Crie uma instância da classe Crawler
     crawler = Crawler()
+    
+    # Execute o método para extrair dados do site
     crawler.extract_from_site()
